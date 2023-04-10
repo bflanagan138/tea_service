c42146d1c32ba055f96e8bb55dbea0782f202db1
Bryan Flanagan <bflanagan78@yahoo.com>
2023-04-10 09:48:41 -0600

    refactor: update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :subscriptions, only: [:create]
+      resources :customers, only: [:index] do
+        resources :subscriptions, only: [:create]
+      end
     end
   end
 end
