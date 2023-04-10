e65fa23b2d91e427951b04a6a2d74a3bc52f0399
Bryan Flanagan <bflanagan78@yahoo.com>
2023-04-10 08:00:44 -0600

    feat: Adds subscriptions controller, sets up post

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,3 +1,12 @@
 class Api::V1::SubscriptionsController < ApplicationController
-  
+  def create
+    require 'pry'; binding.pry
+    subscription_1 = Subscription.new(subscription_params)
+  end
+
+  private
+
+  def subscription_params
+    params.require(:subscription).permit(:title, :price, :frequency, :tea_id)
+  end
 end
--- spec/requests/api/v1/subscriptions_request_spec.rb
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -1,2 +1,20 @@
-require 'rails helper'
+require 'rails_helper'
 
+RSpec.describe "subscriptions" do
+  it 'subscribes a customer to a tea subscription' do
+    customer_1 = Customer.create!(first_name: "Dave", last_name: "Davis", email: "dave@davedavis.com", address: "123 Davis Ct. Davis, MO 66666")
+    tea_1 = Tea.create!(title: "Bengal Spice", description: "Herbal Tea, slightly sweet", temperature: "90", brew_time: "120")
+    # subscription_1 = customer_1.subscriptions.create!(title: "Monthly Harvest", price: "22.48", status: "pending", frequency: "Monthly", tea_id: tea_1[:id])
+    body = {  title: "Monthly Harvest", 
+              price: "22.48", 
+              frequency: "Monthly", 
+              tea_id: tea_1[:id]
+            }
+    headers = { "Content-Type": "application/json", "Accept": "application/json" } 
+
+    post "/api/v1/subscriptions", headers: headers, params: JSON.generate(subscription: body)
+
+    expect(response).to be_successful
+    
+  end
+end
